3dfecd0c579cbd97cc021a0511493b9d264a96a0
margu31 <judypark31@gmail.com>
2021-01-05 15:47:53 +0900

    selection sort ver.2

--- Lesson2/selection-sort.js
+++ Lesson2/selection-sort.js
@@ -8,19 +8,39 @@
 단, 어떠한 빌트인 함수도 사용하지 않고 for 문을 사용하여 구현하여야 한다.
 */
 
+/*
 function selectionSort(array) {
   for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) { 
-      if (array[i] < array[j]) { 
+    let temp = 0;
+    for (let j = i; j < array.length; j++) { 
+      if (array[i] > array[j]) { 
         // 이상하다.. array[i] > array[j]일때 위치가 바뀌어야하는데 그렇게 할땐 결과가 내림차순으로 나옴
         // 0번째 인덱스랑 1번째 인덱스부터 비교 하고 싶어서 j=1로 했는데 또 원하는 결과가 안나옴.. 왜지?
         // 흠.. array[j+1]해도 결과가 안나오고... 뭐람 
-        let temp = 0;
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+
+        // i=0일때는 > 성립하는데 i=1 부터 엇갈리기 시작.... 그래서 j=i해줘야함 j가 i보다 작아지지 않도록!!!
+
+        // temp = array[i];
+        // array[i] = array[j];
+        // array[j] = temp; 
+
+        // 디스트럭처링으로 바꿔보기!
+        [array[i], array[j]] = [array[j], array[i]];
       } 
     }
+  }
+  return array;
+}
+*/
+
+function selectionSort(array) {
+  let min = 0;
+  for (let i = 0; i < array.length; i++) {
+    min = i;
+    for (let j = i+1; j < array.length; j++) {
+      if (array[min] > array[j]) min = j;
+    }
+    [array[i], array[min]] = [array[min], array[i]];
   }
   return array;
 }
